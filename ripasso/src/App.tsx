5b36e6fd83f56a6f6a10a7f51f8f4f68b0db90fd
Marco <marco.zappani@gmail.com>
2022-01-05 17:51:52 +0100

    first commit after setup

---
+++ ripasso/src/App.tsx
@@ -0,0 +1,14 @@
+import { BasicTypes } from "./typescript/BasicTypes";
+
+const App = () => {
+  return (
+    <div className="mt-2">
+      <h1>Introduction to Typescript</h1>
+      <hr/>
+      <BasicTypes />
+    </div>
+  )
+}
+
+
+export default App;
---
+++ ripasso/src/typescript/BasicTypes.tsx
@@ -0,0 +1,7 @@
+export const BasicTypes = () => {
+    return (
+        <>
+          <h3>Basic Types</h3>  
+        </>
+    )
+}
